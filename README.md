82505623a4f5090c87172e52e0c66bde09455927
Warpus <dimanfc1@mail.ru>
2024-06-10 14:34:44 +0700

    microfix

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-1) docker pull rabbitmq
+1) docker pull rabbitmq |
 docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:management
-2) celery -A myshop worker -l info -P eventlet
-3) stripe login
+2) http://127.0.0.1:15672 |
+celery -A myshop worker -l info -P eventlet
+3) stripe login |
 stripe listen --forward-to localhost:8000/payment/webhook/
 4) python manage.py runserver
--- myshop/accounts/urls.py
+++ myshop/accounts/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('profile_form/', views.profile_form, name='profile_form'),
     path('profile/', views.profile, name='profile'),
     path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
